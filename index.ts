038ca9bc93fe1cdef9e1e849fd95998b4e491433
sirily11 <sirily1997@gmail.com>
2020-08-09 19:34:36 +0800

    added index.ts

---
+++ index.ts
@@ -0,0 +1,7 @@
+import { Editor } from "./draft-js-plugins-editor/src";
+import { ColorPickerPlugin } from "./draft-js-color-plugin";
+import { BasePlugin } from "./draft-js-base-plugin";
+import { TextAlignPlugin } from "./draft-js-text-align-plugin"
+
+
+export { Editor, ColorPickerPlugin, BasePlugin, TextAlignPlugin };
